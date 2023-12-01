2598f01a453a30fc1e6a6c8b6a9c940e569e340e
Gavin Morrow <morrowg@gvsu.edu>
2023-11-30 23:02:48 -0500

    moved search bar out of <p></p>, added params, added await to JSON response.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -7,20 +7,30 @@ const SearchBar = () => {
   const handleChange = (e) => {
     setSearchInput(e.target.value);
   };
+  const appKey = "family_fare";
+  const departmentId = "product";
+  const storeId = "6373";
+  const token = "e78b34d2b4e11d4436ab5b4609baade1";
 
   useEffect(() => {
     const getSuggestions = async () => {
       try {
         const response = await fetch(
-          `https://api.freshop.com/1/product_search_suggestions`
+          `https://api.freshop.com/1/product_search_suggestions?app_key=${appKey}&department_id=${departmentId}&q=${searchInput}&store_id=${storeId}&token=${token}`
         );
 
         if (!response.ok) {
           throw new Error("response NOT good");
         }
 
-        const data = response.json();
-        setSuggestions(data.suggestions);
+        const data = await response.json();
+        console.log("API Response:", data);
+
+        if (data && data.suggestions) {
+          setSuggestions(data.suggestions);
+        } else {
+          setSuggestions([]);
+        }
       } catch (error) {
         console.error("Error getting suggestions: ", error);
       }
@@ -32,6 +42,9 @@ const SearchBar = () => {
       setSuggestions([]);
     }
   }, [searchInput]);
+
+  // use for testing
+  // console.log("Current Suggestions:", suggestions);
 
   return (
     <div>
@@ -47,6 +60,12 @@ const SearchBar = () => {
           <li key={index}>{suggestion}</li>
         ))}
       </ul>
+
+      <p>
+        {suggestions.length > 0
+          ? `Suggestions: ${suggestions.join(", ")}`
+          : "No suggestions"}
+      </p>
     </div>
   );
 };
