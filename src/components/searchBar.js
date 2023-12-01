ad8e5858db1ec1378fdce1dc26650196ef37d996
Gavin Morrow <morrowg@gvsu.edu>
2023-11-30 21:00:30 -0500

    added imports and table for testing

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const searchBar = () => {};
-const [searchInput, setSearchInput] = useState("");
+const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-const handleChange = (e) => {
-  e.preventDefault();
-  setSearchInput(e.target.value);
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  useEffect(() => {
+    const getSuggestions = async () => {};
+  });
+
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Continent</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.country}</td>
+              <td>{item.continent}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-// if (searchInput.length > 0) {
-// }
-
-return (
-  <div>
-    <input
-      type="search"
-      placeholder="Search here"
-      onChange={handleChange}
-      value={searchInput}
-    />
-  </div>
-);
+export default SearchBar;
