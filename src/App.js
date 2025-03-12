565fb18d14b975169ffb83c08cc83728c9801cf6
gmorrow <96154802+gamorr@users.noreply.github.com>
2025-03-12 14:32:50 -0400

    remove

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 // App.js
 import React from "react";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/searchBar";
 import handleCreateList from "./components/ShoppingList";
 import ShoppingList from "./components/ShoppingList";
 import {
-  searchBarStyle,
-  shoppingListContainerStyle,
+    searchBarStyle,
+    shoppingListContainerStyle,
 } from "./components/styles";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <p
-          style={{
-            position: "relative",
-            right: "135px",
-            bottom: "785px",
-            fontSize: "20px",
-            fontWeight: "bold",
-            color: "black",
-            textAlign: "center",
-          }}
-        >
-          Product Search
-        </p>
-        <div data-testid="search-bar">
-          <SearchBar style={searchBarStyle} />
+    return (
+        <div className="App">
+            <div>
+                <p
+                    style={{
+                        position: "relative",
+                        right: "135px",
+                        bottom: "785px",
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                        color: "black",
+                        textAlign: "center",
+                    }}
+                >
+                    Product Search
+                </p>
+                <div data-testid="search-bar">
+                    <SearchBar style={searchBarStyle} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
